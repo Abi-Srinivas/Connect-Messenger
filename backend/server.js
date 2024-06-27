0daa9a7d48e12622c7cf9a8505a17d44a8d521d9
Abi-Srinivas <srinivas123a1@gmail.com>
2024-06-27 14:56:52 +0530

    project completed

--- backend/server.js
+++ backend/server.js
@@ -2,35 +2,32 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
-
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+const PORT = process.env.PORT || 4000;
 
 const __dirname = path.resolve();
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
-const PORT = process.env.PORT || 5000;//it was 3000
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
 	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
+
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -63,6 +63,92 @@ const Login = () => {
 };
 export default Login;
 
+// import { useState } from "react";
+// import { Link } from "react-router-dom";
+// import useLogin from "../../hooks/useLogin";
+// import loginVideo from "/assets/loginVideo.mp4";
+
+// const Login = () => {
+//     const [username, setUsername] = useState("");
+//     const [password, setPassword] = useState("");
+
+//     const { loading, login } = useLogin();
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         await login(username, password);
+//     };
+
+//     return (
+//         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+//             {/* Video Background */}
+//             <div className='relative w-full h-screen overflow-hidden'>
+//                 <video
+//                     autoPlay
+//                     loop
+//                     muted
+//                     className='absolute inset-0 w-full h-full object-cover'
+//                     src='/assets/loginVideo.mp4' // Replace with your actual video path
+//                 />
+//                 <div className='w-full h-full bg-gray-400 bg-opacity-50 backdrop-filter backdrop-blur-lg'>
+//                     {/* Your Login Form */}
+//                     <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+//                         <h1 className='text-3xl font-semibold text-center text-gray-300'>
+//                             Login
+//                             <span className='text-green-500'> Connect</span>
+//                         </h1>
+
+//                         <form onSubmit={handleSubmit}>
+//                             <div>
+//                                 <label className='label p-2'>
+//                                     <span className='text-base label-text'>Username</span>
+//                                 </label>
+//                                 <input
+//                                     type='text'
+//                                     placeholder='Enter username'
+//                                     className='w-full input input-bordered h-10'
+//                                     value={username}
+//                                     onChange={(e) => setUsername(e.target.value)}
+//                                 />
+//                             </div>
+
+//                             <div>
+//                                 <label className='label'>
+//                                     <span className='text-base label-text'>Password</span>
+//                                 </label>
+//                                 <input
+//                                     type='password'
+//                                     placeholder='Enter Password'
+//                                     className='w-full input input-bordered h-10'
+//                                     value={password}
+//                                     onChange={(e) => setPassword(e.target.value)}
+//                                 />
+//                             </div>
+//                             <Link
+//                                 to='/signup'
+//                                 className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'
+//                             >
+//                                 {"Don't"} have an account?
+//                             </Link>
+
+//                             <div>
+//                                 <button className='btn btn-block btn-sm mt-2' disabled={loading}>
+//                                     {loading ? (
+//                                         <span className='loading loading-spinner '></span>
+//                                     ) : (
+//                                         "Login"
+//                                     )}
+//                                 </button>
+//                             </div>
+//                         </form>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Login;
 
 
 
